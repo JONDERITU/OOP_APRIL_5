e85cc05ff4d8fe6ff6b3b6ff2b2468dcefef0281
INTERN TWO <Interntwo@JGichane.connectify>
2016-04-05 23:15:56 +0300

    finalised on index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//index.php 
+
+require_once 'includes/global.inc.php';
+?>
+
+<html>
+<head>
+	<title>Homepage</title>
+</head>
+<body>
+
+<?php if(isset($_SESSION['logged_in'])) : ?>
+	<?php $user = unserialize($_SESSION['user']); ?>
+	Hello, <?php echo $user->username; ?>. You are logged in. <a href="logout.php">Logout</a> | <a href="settings.php">Change Email</a>
+<?php else : ?>
+	You are not logged in. <a href="login.php">Log In</a> | <a href="register.php">Register</a>
+<?php endif; ?>
+
+</body>
+</html>
