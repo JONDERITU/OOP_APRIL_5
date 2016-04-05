753665f8bb100f392e22a5380330670e55d36896
INTERN TWO <Interntwo@JGichane.connectify>
2016-04-05 23:08:04 +0300

    added globalinc.php required by every page in the app

---
+++ includes/global.inc.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'classes/User.class.php';
+require_once 'classes/UserTools.class.php';
+require_once 'classes/DB.class.php';
+
+//connect to the database
+$db = new DB();
+$db->connect();
+
+//initialize UserTools object
+$userTools = new UserTools();
+
+//start the session
+session_start();
+
+//refresh session variables if logged in
+if(isset($_SESSION['logged_in'])) {
+	$user = unserialize($_SESSION['user']);
+	$_SESSION['user'] = serialize($userTools->get($user->id));
+}
+?>
